4f181ff3875dd0c0d50cfcada153b438f827f611
Mikolaj Kojdecki <mikolaj.kojdecki@polidea.com>
2020-04-04 15:57:58 +0200

    Dodanie walidacji długości kodu SMS

--- app/src/main/kotlin/pl/gov/mc/protego/ui/registration/RegistrationConfirmationActivity.kt
+++ app/src/main/kotlin/pl/gov/mc/protego/ui/registration/RegistrationConfirmationActivity.kt
@@ -3,6 +3,7 @@ package pl.gov.mc.protego.ui.registration
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import kotlinx.android.synthetic.main.registration_confirmation_view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import pl.gov.mc.protego.R
@@ -18,21 +19,28 @@ class RegistrationConfirmationActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registration_confirmation_view)
 
+        sms_code.doOnTextChanged { text, _, _, _ -> viewModel.onCodeChanged(text.toString()) }
+
         confirm_registration_button.setOnClickListener {
-            if (sms_code.text.toString().isNotEmpty()) {
-                viewModel.confirm(sms_code.text.toString())
-            } else {
-                sms_code_layout.error = "Wpisz kod"
+            viewModel.confirm(sms_code.text.toString())
+        }
+
+        with(viewModel) {
+            observeLiveData(confirmationEnabled) { confirm_registration_button.isEnabled = it }
+
+            observeLiveData(confirmationError) {
+                Toast.makeText(
+                    this@RegistrationConfirmationActivity,
+                    "Problem z rejestracją: $it",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            observeLiveData(confirmationSuccess) {
+                navigateToMain()
             }
         }
 
-        observeLiveData(viewModel.confirmationError) {
-            Toast.makeText(this, "Problem z rejestracją: $it", Toast.LENGTH_LONG).show()
-        }
-
-        observeLiveData(viewModel.confirmationSuccess) {
-            navigateToMain()
-        }
     }
 
     private fun navigateToMain() {
